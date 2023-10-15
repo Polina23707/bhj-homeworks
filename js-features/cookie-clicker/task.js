14d3d14367cb565c4b5459910406a3c6f762ded2
Polina Gorelik <Polina23707@gmail.com>
2023-10-15 14:26:33 +0300

    second commit for cookie-clicker

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,13 +1,6 @@
 const clickerCounter = document.getElementById("clicker__counter");
 const cookie = document.getElementById("cookie");
-let counter = 0;
-cookie.onclick = function () {
-  counter += 1;
-  clickerCounter.textContent = counter;
 
-  if (clickerCounter.textContent % 2 === 0) {
-    cookie.width = 200;
-  } else {
-    cookie.width = 230;
-  }
+cookie.onclick = function () {
+  cookie.width = ++clickerCounter.textContent % 2 ? 250 : 200;
 }

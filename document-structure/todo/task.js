e6de2484c266be3bc07c80d4ff8fc58cafe2e802
Polina Gorelik <Polina23707@gmail.com>
2023-10-19 15:27:47 +0300

    second commit for todo

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,31 +1,43 @@
 let input = document.getElementById('task__input');
 let tasklist = document.getElementById('tasks__list');
+let add = document.getElementById('tasks__add');
+
+let remove = document.createElement('a');
+
+add.addEventListener('click', (e) => addTask(e));
 
 document.addEventListener('keyup', (e) => {
   if (e.code === 'Enter') {
     e.preventDefault();
-    let task = document.createElement('div');
-    tasklist.appendChild(task);
-
-    task.outerHTML = `<div class="task">
-    <div class="task__title">
-      ${input.value}
-    </div>
-    <a href="#" class="task__remove">&times;</a>
-    </div>`;
-    input.value = '';
-
-    const removes = Array.from(document.querySelectorAll('.task__remove'));
-
-    removes.forEach((remove) => {
-      remove.onclick = function () {
-        console.log('remove');
-        console.log(remove.parentElement.className);
-        remove.parentElement.remove();
-      }
-    })
+    addTask(e);
   }
 });
+
+function addTask(event) {
+  event.preventDefault();
+
+  if (input.value.trim()) {
+    let task = document.createElement('div');
+    tasklist.appendChild(task);
+    task.classList.add('task');
+
+    let taskTitle = document.createElement('div');
+    task.appendChild(taskTitle);
+    taskTitle.classList.add('task__title');
+    taskTitle.textContent = input.value;
+    
+    task.insertAdjacentElement('beforeEnd', remove);
+    remove.outerHTML = '<a href="#" class="task__remove">&times;</a>'
+    console.log(task.lastChild);
+
+    task.lastChild.onclick = function () {
+      console.log('delete');
+      task.remove();
+    }
+    input.value = '';
+  }
+}
+
 
 
 

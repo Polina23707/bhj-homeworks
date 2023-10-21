49b879593c7ebdad99f6a670bbd0a9aee5614ebd
Polina Gorelik <Polina23707@gmail.com>
2023-10-21 10:08:45 +0300

    second commit for auth

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -4,27 +4,40 @@ const welcome = document.getElementById('welcome');
 const signin = document.getElementById('signin');
 const input = Array.from(document.querySelectorAll('.control'));
 
-document.getElementById('signin__form').addEventListener('submit', (e) => {
-  e.preventDefault()
-  const xhr = new XMLHttpRequest();
+document.addEventListener('DOMContentLoaded', () => {
+  if (localStorage.id) {
+    signin.classList.toggle('signin_active');
+    welcome.classList.toggle('welcome_active');
+    welcome.firstElementChild.textContent = localStorage.id;
 
-  xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth', true);
-  var formData = new FormData(form);
-  xhr.send(formData);
+  } else {
 
-  xhr.onload = function() {
-    let answer = JSON.parse(xhr.responseText)
-
-    if (answer.success) {
-      welcome.classList.toggle('welcome_active');
-      signin.classList.toggle('signin_active');
-
-      welcome.firstElementChild.setAttribute('id', answer.user_id);
-      welcome.firstElementChild.textContent = answer.user_id;
-    } else {
-      alert('Неверный пользователь / пароль!');
-      input[0].value = '';
-      input[1].value = '';
-    }
+    document.getElementById('signin__form').addEventListener('submit', (e) => {
+      e.preventDefault()
+      const xhr = new XMLHttpRequest();
+    
+      xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth', true);
+      var formData = new FormData(form);
+      xhr.responseType = 'json';
+      xhr.send(formData);
+    
+      xhr.onload = function() {
+        let answer = xhr.response
+    
+        if (answer.success) {
+          welcome.classList.toggle('welcome_active');
+          signin.classList.toggle('signin_active');
+    
+          welcome.firstElementChild.setAttribute('id', answer.user_id);
+          welcome.firstElementChild.textContent = answer.user_id;
+          
+          localStorage.setItem('id', answer.user_id);
+        } else {
+          alert('Неверный пользователь / пароль!');
+          form.reset();
+        }
+      }
+    })
   }
-})
+});
+

cc4af4debe46e2135c86a78a231b2c1b58fed7ac
Polina Gorelik <Polina23707@gmail.com>
2023-10-21 11:59:09 +0300

    3rd commit for auth

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -5,14 +5,14 @@ const signin = document.getElementById('signin');
 const input = Array.from(document.querySelectorAll('.control'));
 
 document.addEventListener('DOMContentLoaded', () => {
-  if (localStorage.id) {
-    signin.classList.toggle('signin_active');
-    welcome.classList.toggle('welcome_active');
+    signin.classList.remove('signin_active');
+    welcome.classList.add('welcome_active');
     welcome.firstElementChild.textContent = localStorage.id;
 
-  } else {
-
-    document.getElementById('signin__form').addEventListener('submit', (e) => {
+  if (!localStorage.id) {
+      signin.classList.add('signin_active');
+      welcome.classList.remove('welcome_active');
+      document.getElementById('signin__form').addEventListener('submit', (e) => {
       e.preventDefault()
       const xhr = new XMLHttpRequest();
     
@@ -25,19 +25,21 @@ document.addEventListener('DOMContentLoaded', () => {
         let answer = xhr.response
     
         if (answer.success) {
-          welcome.classList.toggle('welcome_active');
-          signin.classList.toggle('signin_active');
+          welcome.classList.add('welcome_active');
+          signin.classList.remove('signin_active');
     
           welcome.firstElementChild.setAttribute('id', answer.user_id);
           welcome.firstElementChild.textContent = answer.user_id;
-          
+
           localStorage.setItem('id', answer.user_id);
         } else {
           alert('Неверный пользователь / пароль!');
           form.reset();
+          return;
         }
       }
     })
+
   }
 });
 
